7d6bdd0a48335dc13b20b9b630568bc16271ef18
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2021-04-03 14:04:48 +0530

    Procfile added

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Time_Series_Application.wsgi
--- Time_Series_Application/settings.py
+++ Time_Series_Application/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='6s$an^9j1l746sw3!8@a#p-@!$g)r!k$+7n7=!nzgg(a$+hx9n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = True
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','timeseries-application.herokuapp.com']
 
 
 # Application definition
