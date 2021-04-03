eb34e78b419912c18422562cf33a9d394bc6d731
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2021-04-03 14:13:05 +0530

    Added django-heroku

--- Time_Series_Application/settings.py
+++ Time_Series_Application/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from pathlib import Path
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,5 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+django_heroku.settings(locals())
