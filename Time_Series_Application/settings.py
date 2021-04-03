715499d35e335cb8614ba99919ad5b1b17e42d76
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2021-04-03 14:46:36 +0530

    Settings

--- Time_Series_Application/settings.py
+++ Time_Series_Application/settings.py
@@ -124,6 +124,12 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 django_heroku.settings(locals())
