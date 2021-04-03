663ad714e0af974c38de0010aae78d35ad008021
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2021-04-03 12:06:58 +0530

    Added env

--- Time Series Application/Time_Series_Application/settings.py
+++ Time Series Application/Time_Series_Application/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6s$an^9j1l746sw3!8@a#p-@!$g)r!k$+7n7=!nzgg(a$+hx9n'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+
 
 ALLOWED_HOSTS = []
 
