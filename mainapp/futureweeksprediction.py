d2c27fa2e431f95ccf4e47ea3731e7114efa9f7b
sauravjoshi23 <sauravjoshi2362000@gmail.com>
2021-04-03 16:59:12 +0530

    pickle error

--- mainapp/futureweeksprediction.py
+++ mainapp/futureweeksprediction.py
@@ -1,5 +1,5 @@
 import os
-#import pickle
+import pickle
 import pandas as pd
 
 
@@ -8,13 +8,13 @@ def futureweeksprediction(n, ci):
 
     ci = (100-ci)*0.01
 
-    # with open('model_superstore_sales.pkl', 'rb') as file:
-    #     print('Model Loaded')
-    #     model = pickle.load(file)
-    #     print(model)
+    with open('model_superstore_sales.pkl', 'rb') as file:
+        print('Model Loaded')
+        model = pickle.load(file)
+        print(model)
 
-    print('Model Loaded Future')
-    model = pd.read_pickle('model_superstore_sales.pkl')
+    # print('Model Loaded Future')
+    # model = pd.read_pickle('model_superstore_sales.pkl')
 
     forecast = model.forecast(steps=n) # making a forecast of n weeks later of the last date in the 'Date' column
 
